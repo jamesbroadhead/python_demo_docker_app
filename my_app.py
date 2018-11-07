e73f8961acf14b5e2e7db5fed4da65b613b8935b
James Broadhead <james.broadhead@mongodb.com>
2018-11-07 22:20:24 +0000

    gci autocommit

--- my_app.py
+++ my_app.py
@@ -11,8 +11,8 @@ db = client.test_database
 
 while True:
     time.sleep(0.1)
+    dt = datetime.datetime.utcnow()
     try:
-        dt = datetime.datetime.utcnow()
         post = {"author": "James",
                 "text": "I wrote to a database on kubernetes",
                 "date": dt}
@@ -20,6 +20,6 @@ while True:
         result = db.posts.insert_one(post)
         print('Posted successfully - {} at {}'.format(result.inserted_id, dt))
     except Exception as e:
-        print('Failed to post - {}'.format(e))
+        print('Failed to post - {} - at {}'.format(e, dt))
 
 print("Exiting")
