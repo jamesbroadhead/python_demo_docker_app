38a549edee9e1d1732118ad70d08b2e7678be404
James Broadhead <james.broadhead@mongodb.com>
2018-11-27 10:01:09 +0000

    redeploying

--- my_app.py
+++ my_app.py
@@ -6,16 +6,22 @@ import time
 
 print("Starting up the app!")
 
-client = pymongo.MongoClient('my-replica-set-0.my-replica-set-svc.jbo.svc.cluster.local:27017', serverSelectionTimeoutMS=500, connectTimeoutMS=500, socketTimeoutMS=500)
+client = pymongo.MongoClient(
+    'my-replica-set-0.my-replica-set-svc.jbo.svc.cluster.local:27017',
+    serverSelectionTimeoutMS=500,
+    connectTimeoutMS=500,
+    socketTimeoutMS=500)
 db = client.test_database
 
 while True:
     time.sleep(0.1)
     dt = datetime.datetime.utcnow()
     try:
-        post = {"author": "James",
-                "text": "I wrote to a database on kubernetes",
-                "date": dt}
+        post = {
+            "author": "James",
+            "text": "I wrote to a database on kubernetes",
+            "date": dt
+        }
 
         result = db.posts.insert_one(post)
         print('Posted successfully - {} at {}'.format(result.inserted_id, dt))
